52ed02029763e3794421f200b3bc7c701c988166
CG3-C0ding-W <121372008+CG3-C0ding-W@users.noreply.github.com>
2023-04-04 21:58:02 -0700

    Update RMCharacterListViewViewModel.swift

--- RickAndMorty/ViewModels/RMCharacterListViewViewModel.swift
+++ RickAndMorty/ViewModels/RMCharacterListViewViewModel.swift
@@ -10,6 +10,8 @@ import UIKit
 
 protocol RMCharacterListViewViewModelDelegate: AnyObject {
     func didLoadInitialCharacters()
+    func didLoadMoreCharacters(with newIndexPaths: [IndexPath])
+    
     func didSelectCharacter(_ character: RMCharacter)
 }
 
@@ -22,13 +24,16 @@ final class RMCharacterListViewViewModel: NSObject {
     
     private var characters: [RMCharacter] = [] {
         didSet {
+            print("Creating viewModels")
             for character in characters {
                 let viewModel =  RMCharacterCollectionViewCellViewModel(
                     characterName: character.name,
                     characterStatus: character.status,
                     characterImageUrl: URL(string: character.image)
                 )
-                cellViewModels.append(viewModel)
+                if !cellViewModels.contains(viewModel) {
+                    cellViewModels.append(viewModel)
+                }
             }
         }
     }
@@ -60,22 +65,44 @@ final class RMCharacterListViewViewModel: NSObject {
     
     ///Paginate if additional characters are needed
     public func fetchAdditionalCharacters(url: URL) {
-        isLoadingMoreCharacters = true
-        print("Fetching more characters")
-        
-        guard let request = RMRequest(url: url) else {
-            isLoadingMoreCharacters = false
-            print("Failed to create request ")
+        guard !isLoadingMoreCharacters else {
             return
         }
         
-        RMService.shared.execute(request,
-                                 expecting: RMGetAllCharactersResponse.self) { result in
+        isLoadingMoreCharacters = true
+        guard let request = RMRequest(url: url) else {
+            isLoadingMoreCharacters = false
+            return
+        }
+        
+        RMService.shared.execute(request, expecting: RMGetAllCharactersResponse.self) { [weak self] result in
+            guard let strongSelf = self else {
+                return
+            }
             switch result {
-            case .success(let success):
-                print(String(describing: success))
+            case .success(let responseModel):
+                let moreResults = responseModel.results
+                let info = responseModel.info
+                strongSelf.apiInfo = info
+                
+                let originalCount = strongSelf.characters.count
+                let newCount = moreResults.count
+                let total = originalCount + newCount
+                let startingIndex = total - newCount
+                let indexPathstoAdd: [IndexPath] = Array(startingIndex..<(startingIndex + newCount)).compactMap({
+                    return IndexPath(row: $0, section: 0)
+                })
+            
+                strongSelf.characters.append(contentOf: moreResults)
+                DispatchQueue.main.async {
+                    strongSelf.delegate?.didLoadMoreCharacters(
+                        with: indexPathsToAdd
+                    )
+                    strongSelf.isLoadingMoreCharacters = false
+                }   
             case .failure
                 print(String(describing: failure))
+                self?.isLoadingMoreCharacters = false
             }
         }
         
