e118eb324768aa8d5c10c9b5851cf3b0de9925e7
CG3-C0ding-W <121372008+CG3-C0ding-W@users.noreply.github.com>
2023-04-06 18:55:12 -0700

    Update RMCharacterDetailViewViewModel.swift

--- RickAndMorty/ViewModels/RMCharacterDetailViewViewModel.swift
+++ RickAndMorty/ViewModels/RMCharacterDetailViewViewModel.swift
@@ -14,7 +14,20 @@ final class RMCharacterDetailViewViewModel {
         self.character = character
     }
     
+    private var requestUrl: URL? {
+        return URL(string: character.url)
+    }
+    
     public var title: String {
         character.name.uppercased()
+    }
+    
+    public func fetchCharacterData() {
+        print(character.url)
+        guard let url = requestUrl,
+            request = RMRequest(url: url) else {
+            print("Failed to create")
+          return
+        }
     }
 }
